e3177cc4a46baa4cdae319a25d5f5d5eb759d9ad
El-Partu <laurentpartu@gmail.com>
2025-11-21 18:30:15 +0000

    enforcing https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,23 @@
 <?php
-
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void {
+		if (env('APP_ENV') !== 'local') {
+			URL::forceScheme('https');
+		}
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void {
+		//
+	}
 }
