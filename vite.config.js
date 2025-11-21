ca80eab2a3dd8786ab9083c90e844b0b2c40ad68
El-Partu <laurentpartu@gmail.com>
2025-11-21 18:01:32 +0000

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,32 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//       ],
+//     });
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: process.env.NODE_ENV === 'production' ? process.env.APP_URL + '/' : '/',
 });
